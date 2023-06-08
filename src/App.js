c4ce0937b693572ef46dbe729aed31e584ab0f28
janani <jayaramanjanani73@gmail.com>
2023-06-08 09:59:32 +0530

    added code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,171 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import sound from "./music/notification.mp3"
+import "./styles/TouchTypingApp.css"
 
-function App() {
+const keysToPractice = ['a','s','k',' ' ,
+'d','a','d' ,' ' ,
+'s ' ,' a' ,'l ' ,'a ' ,' d' ,' s' ,' ' ,
+' f' ,'a ' ,' l' ,'l ' ,' ' ,
+' l' ,' a' ,'s ' ,' s' ,' ',
+'a ',' l','l ',' ',
+' a','d ','s ',' ',
+' a' ,'d ' ,' d' ,' ' ,
+// 's ' ,'a ' ,' l' ,' s' ,'a ' ,
+// 'j ' ,'a ' ,'f ' ,'f ' ,  ' ' ,
+// 'k ' ,'a ' ,'d ' ,' ' ,
+// 'f ' ,' l' ,'a ' ,' s' ,' k ' ,' ' ,
+// 's ' ,' a' ,' d' ,' ' ,
+// 'a ' ,' l' ,' f' ,' a' ,' '
+];
+// 
+//   add salsa jaff kad; flask lass sad fall alfa jak kaj dad ask
+const TouchTypingApp = () => {
+  const [typedText, setTypedText] = useState('');
+  const [accuracy, setAccuracy] = useState(100);
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(0);
+  const [currentKeyIndex, setCurrentKeyIndex] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(300);
+  const [correctWordsTyped, setCorrectWordsTyped] = useState(0);
+  const [timerRunning, setTimerRunning] = useState(false);
+  const notificationSoundRef = useRef(null);
+
+  const startTimer = () => {
+    setTypedText('');
+    setAccuracy(100);
+    setRemainingTime(300);
+    setTimerRunning(true);
+    setStartTime(Date.now());
+  };
+
+  useEffect(() => {
+    if (timerRunning) {
+      const timer = setInterval(() => {
+        setRemainingTime((prevTime) => {
+          if (prevTime === 1) {
+            setEndTime(Date.now());
+            clearInterval(timer);
+            setTimerRunning(false);
+            window.alert("Time's up!");
+          }
+          return prevTime - 1;
+        });
+      }, 1000);
+
+      return () => {
+        clearInterval(timer);
+      };
+    }
+  }, [timerRunning]);
+
+  useEffect(() => {
+    if (typedText.length < keysToPractice.length) {
+      setCurrentKeyIndex(typedText.length);
+    }
+    if (accuracy < 100) {
+      notificationSoundRef.current.play();
+    }
+  }, [typedText, accuracy]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Backspace') {
+      e.preventDefault();
+      setTypedText((prevText) => prevText.slice(0, -1));
+      return;
+    }
+
+    const keyPressed = e.key.toLowerCase();
+    setTypedText((prevText) => prevText + keyPressed);
+    setAccuracy(calculateAccuracy(keyPressed));
+  };
+
+  
+
+  const calculateAccuracy = (keyPressed) => {
+    const totalKeystrokes = typedText.length + 1;
+    const incorrectKeystrokes = [...typedText, keyPressed].reduce(
+      (count, char, index) => (char !== keysToPractice[index] ? count + 1 : count),
+      0
+    );
+    const calculatedAccuracy = ((totalKeystrokes - incorrectKeystrokes) / totalKeystrokes) * 100;
+    return calculatedAccuracy.toFixed(2);
+  };
+
+  const calculateTypingSpeed = () => {
+    const wordsTyped = typedText.trim().split(' ').length;
+    const elapsedTimeInMinutes = (endTime - startTime) / 1000 / 60;
+    const wordsPerMinute = (wordsTyped / elapsedTimeInMinutes).toFixed(0);
+    return wordsPerMinute;
+  };
+
+  const getKeyColor = (index) => {
+    if (typedText.length > index) {
+      return typedText[index] === keysToPractice[index] ? 'green' : 'red';
+    }
+    if (index === currentKeyIndex) {
+      return 'yellow';
+    }
+    return 'white';
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
+
+  useEffect(() => {
+    if (remainingTime === 0) {
+      setTimerRunning(false);
+    }
+  }, [remainingTime]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="touch-typing-app">
+      
+      <div style={{border:"0px solid blue",width:"70%", margin:"auto"}} >
+      <h1>Touch Typing Practice</h1>
+      {!timerRunning ? (
+        <button className="start-button" onClick={startTimer}>
+          Start
+        </button>
+      ) : (
+        <div className="keyboard-container">
+          <div style={{border:"0px solid green",width:"100%",display:"flex"}}>
+          {keysToPractice.map((key, index) => (
+            <span
+              key={index}
+              className={`key ${getKeyColor(index)}`}
+            >
+              {key}
+            </span>
+          ))}
+          </div>
+        </div>
+      )}
+      </div>
+      <audio ref={notificationSoundRef} src={sound} />
+      <textarea className="input-area" onKeyDown={handleKeyDown} value={typedText} readOnly />
+      <div className="stats-container">
+        <div className="stat-item">
+          <span className="stat-label">Accuracy:</span>
+          <span className="stat-value">{accuracy}%</span>
+        </div>
+        <div className="stat-item">
+          <span className="stat-label">Typing Speed:</span>
+          <span className="stat-value">{calculateTypingSpeed()} WPM</span>
+        </div>
+        <div className="stat-item">
+          <span className="stat-label">Time Remaining:</span>
+          <span className="stat-value">{formatTime(remainingTime)}</span>
+        </div>
+        <div className="stat-item">
+          <span className="stat-label">Correct Words Typed:</span>
+          <span className="stat-value">{correctWordsTyped}</span>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default TouchTypingApp;
